bba710dc9d24bd099dfc71d216929d937182b0d1
stelar7 <dudedbz@gmail.com>
2024-12-31 03:34:08 +0100

    LibWeb: Implement check_that_a_key_could_be_injected_into_a_value for IndexedDB

--- Libraries/LibWeb/IndexedDB/Internal/Algorithms.h
+++ Libraries/LibWeb/IndexedDB/Internal/Algorithms.h
@@ -37,5 +37,6 @@ GC::Ref<HTML::DOMStringList> create_a_sorted_name_list(JS::Realm&, Vector<String
 WebIDL::ExceptionOr<JS::Value> clone_in_realm(JS::Realm&, JS::Value, GC::Ref<IDBTransaction>);
 WebIDL::ExceptionOr<ErrorOr<Key>> extract_a_key_from_a_value_using_a_key_path(JS::Realm&, JS::Value, KeyPath, bool = false);
 WebIDL::ExceptionOr<ErrorOr<JS::Value>> evaluate_key_path_on_a_value(JS::Realm&, JS::Value, KeyPath);
+bool check_that_a_key_could_be_injected_into_a_value(JS::Realm&, JS::Value, KeyPath);
 
 }
