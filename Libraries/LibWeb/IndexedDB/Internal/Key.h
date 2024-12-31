e657617629027d91edf54fac335fb8dd1179fdd5
stelar7 <dudedbz@gmail.com>
2024-12-31 03:25:54 +0100

    LibWeb: Implement extract_a_key_from_a_value_using_a_key_path for IndexedDB

--- Libraries/LibWeb/IndexedDB/Internal/Key.h
+++ Libraries/LibWeb/IndexedDB/Internal/Key.h
@@ -66,6 +66,11 @@ public:
     [[nodiscard]] static bool less_than(GC::Ref<Key> a, GC::Ref<Key> b) { return compare_two_keys(a, b) < 0; }
     [[nodiscard]] static bool greater_than(GC::Ref<Key> a, GC::Ref<Key> b) { return compare_two_keys(a, b) > 0; }
 
+    bool operator==(Key const& other) const
+    {
+        return Key::compare_two_keys(*this, other) == 0;
+    }
+
 private:
     Key(KeyType type, KeyValue value)
         : m_type(type)
