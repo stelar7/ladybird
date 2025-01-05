d43a4714c6d1a81648a4ac3ad2a90969e3fd08e6
stelar7 <dudedbz@gmail.com>
2025-01-05 20:37:25 +0100

    LibWeb: Implement some more of store_a_record_into_an_object_store

--- Libraries/LibWeb/IndexedDB/Internal/Algorithms.h
+++ Libraries/LibWeb/IndexedDB/Internal/Algorithms.h
@@ -42,7 +42,8 @@ WebIDL::ExceptionOr<JS::Value> clone_in_realm(JS::Realm&, JS::Value, GC::Ref<IDB
 WebIDL::ExceptionOr<ErrorOr<Key>> extract_a_key_from_a_value_using_a_key_path(JS::Realm&, JS::Value, KeyPath, bool = false);
 WebIDL::ExceptionOr<ErrorOr<JS::Value>> evaluate_key_path_on_a_value(JS::Realm&, JS::Value, KeyPath);
 bool check_that_a_key_could_be_injected_into_a_value(JS::Realm&, JS::Value, KeyPath);
-WebIDL::ExceptionOr<Optional<Key>> store_a_record_into_an_object_store(GC::Ref<IDBObjectStore>, JS::Value, Optional<Key>, bool);
+WebIDL::ExceptionOr<Optional<Key>> store_a_record_into_an_object_store(JS::Realm&, GC::Ref<IDBObjectStore>, JS::Value, Optional<Key>, bool);
+void delete_records_from_an_object_store(GC::Ref<IDBObjectStore>, GC::Ref<IDBKeyRange>);
 WebIDL::ExceptionOr<u64> generate_a_key(GC::Ref<IDBObjectStore>);
 void inject_a_key_into_a_value_using_a_key_path(JS::Realm&, JS::Value, Key, KeyPath);
 void possibly_update_the_key_generator(GC::Ref<IDBObjectStore>, Key);
